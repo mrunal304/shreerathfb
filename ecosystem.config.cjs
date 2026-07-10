797e4557c0c0eca1f66f1160e5e05f91694c06ab
mrunal304 <mrunaln304@gmail.com>
2026-07-10 17:32:44 +0530

    Fix formatting in ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -12,7 +12,7 @@ module.exports = {
       MONGODB_URI: "mongodb+srv://Mrunali:Mrunalifeedback@feedbackqrform.fbhwhe8.mongodb.net/shreerathqr?appName=feedbackqrform",
       SESSION_SECRET: "TH44OuZsO8Eo1xeojLGNfJYBIhv6oANERF57ZpijD2ALHThp2a0zoghjJDW1ygylOUgOoaT5xY2kwFmIxHFF9w==",
       ADMIN_USERNAME: "admin",
-      ADMIN_PASSWORD: "shreerath_admin_2026"
+      ADMIN_PASSWORD: "shreerath_admin_2026",
       TRUST_PROXY: "1",
     }
   }]
